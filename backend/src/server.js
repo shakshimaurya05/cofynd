96ba9b07fef22985fc69b0a8e47c21cff99658d2
Vishesh07s <visheshsingh074@gmail.com>
2026-01-12 14:14:43 +0530

    mongoDb connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,3 +4,14 @@ require('dotenv').config();
 const app = express();
 const PORT = 5000;
 app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res) => {
+  res.json({
+    message : "Bckend server is running"
+  });
+});
+
+app.listen(PORT,() => {
+  console.log(`Server is running on port ${PORT}`);
+})
