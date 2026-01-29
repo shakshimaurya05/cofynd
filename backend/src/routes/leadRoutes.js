44bb8e5901ed17dbfcffd86da57c9990d491e47f
Vishesh07s <visheshsingh074@gmail.com>
2026-01-29 14:55:18 +0530

    leadRoutes.js updated

--- backend/src/routes/leadRoutes.js
+++ backend/src/routes/leadRoutes.js
@@ -1,40 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const Lead = require('../models/Lead');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-//POST a new lead
+//create a transporter for sending mails
+const transporter =  nodemailer.createTransport({
+  service : "gmail",
+  auth: {
+    user : process.env.MAIL_USER,
+    pass : process.env.MAIL_PASS
+  }
+});
+
+//POST A NEW LEAD
 router.post('/',async(req,res) => {
   try{
     const leadData = {
       name : req.body.name,
-      email : req.body.email,
+      email: req.body.email,
       phone : req.body.phone,
       spaceType : req.body.spaceType,
-      city : req.body.city
+      city: req.body.city
     };
-    
-    const savedLead = await Lead.create(leadData);
-    res.status(201).json(savedLead);
 
+    const savedLead = await Lead.create(leadData);
+
+    //send mail to admin
+    const mailOptions = {
+      from : process.env.MAIL_USER,
+      to : process.env.MAIL_USER,
+      subject : 'New Lead Submission - CoworkSpaze',
+      html : `
+      <h2> New Lead Submitted </h2>
+      <p><strong> Name : </strong> ${leadData.name} </p>
+      <p><strong> Email : </strong> ${leadData.email} </p>
+      <p><strong> Phone : </strong> ${leadData.phone} </p>
+      <p><strong> City : </strong> ${leadData.city} </p>
+      <p><strong> Submitted at : </strong> ${new Date().toLocaleString()}</p>
+      `
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(201).json(savedLead);
   }
   catch(err){
     res.status(400).json({
       message : err.message
     });
   }
-});
+})
 
-//GET all leads only for admin 
-router.get('/',async(req,res) => {
-  try{
-   const leads = await Lead.find().sort({ createdAt: -1 });
-//show leads in descending order(most recent first)
-   //.sort() is a mongoose query method
-    res.json(leads);
-  }
-  catch(err){
-    res.status(500).json({message : err.message});
-  }
-});
-
-module.exports = router;
