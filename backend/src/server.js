d04dd771bea12f8d290253019fe49c2a1a2613e4
Vishesh07s <visheshsingh074@gmail.com>
2026-01-29 23:57:22 +0530

    added rate limiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors'); //CROSS ORIGIN RESOURCE SHARING
 require('dotenv').config();
+const rateLimit = require('express-rate-limit');
 
 
 //connect to MongoDB
@@ -16,10 +17,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cors());
 
+//rate limiting for lead submissions
+const leadSubmissionLimiter = rateLimit({
+  windowMs: 60*60*1000, //60 mins
+  max: 3, //limit each IP to maximum 3 requests per windowMs
+  message : 'Too many lead submissions, please try again later',
+  standardHeaders: true,
+  legacyHeaders : false
+})
+
+app.use('/api/leads', leadSubmissionLimiter);
+
 //import and use space routes
 const spaceRoutes = require('./routes/spaceRoutes');
 app.use('/api/spaces',spaceRoutes);
-
 //import and use lead routes
 const leadRoutes = require('./routes/leadRoutes');
 app.use('/api/leads' , leadRoutes);
